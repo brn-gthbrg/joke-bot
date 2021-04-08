d74e2c34281328351a8dcbee576d13c9a848ef67
Brian Gothberg <btgothberg@gmail.com>
2021-04-07 19:56:47 -0700

    push with heroku

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/bwmarrin/discordgo v0.23.2 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/jokeapi-go v0.0.0-00010101000000-000000000000 // indirect
 )
 
--- main.go
+++ main.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 	"github.com/jokeapi-go"
 )
 
-var token = os.Getenv("DISCORD_TOKEN")
+// const token = DISCORD_TOKEN
 
 var BotID string
 
+func keyReader(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func main() {
 	// Create a new Discord session using the provided bot token.
+	token := keyReader("DISCORD_KEY")
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
@@ -64,6 +79,10 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// If the message is "ping" reply with "Pong!"
 	if strings.ToLower(m.Content) == "tell me a joke" {
 		s.ChannelMessageSend(m.ChannelID, joke)
+	}
+
+	if strings.ToLower(m.Content) == "tell me your best joke" {
+		s.ChannelMessageSend(m.ChannelID, "I'm an unskilled bot with no concept of best")
 	}
 
 }
